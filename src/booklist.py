175b17b7578746b71b552f7f78e2c5fbcbc83432
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2018-06-19 01:29:34 +0100

    Added the main driver - booklist.py.

--- src/booklist.py
+++ src/booklist.py
@@ -1,8 +1,105 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup as bsoup
+from fake_useragent import UserAgent
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from functools import lru_cache
+import locale
+import src.models
+
+# Set default locale - required while parsing string to float (later)
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+# =================================================================================
+# Database setup
+# =================================================================================
+
+# Base class for the rest of the models
+Base = declarative_base()
+Session = sessionmaker()
+
+
+def setup_database(reset=False):
+    """
+    Performs initial setup of the database
+    :param reset: if true, deletes the records and starts from scratch
+    :return: nothing
+    """
+    engine = create_engine('sqlite://book.db', echo=True)
+    Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
+
+
+# =================================================================================
+# Download URLs
+# =================================================================================
+
+@lru_cache(maxsize=128)
+def get_request_headers():
+    headers = {
+        'User-Agent': str(UserAgent().chrome)
+    }
+    return headers
+
+
+def download_url_html(url):
+    headers = get_request_headers()
+    # r = requests.get(url, headers=headers)
+    # if r.status_code == 200:
+    #     html = r.text
+    with open('out/index.html') as f:
+        html = f.read()
+    return html
+
+
+def parse_html_and_add_record(html):
+    s = bsoup(html, 'lxml')
+    container = s.find('div', class_='leftContainer')
+    category_books_count = locale.atoi(container.find('span', class_='smallText').text.strip().rstrip(')').split()[-1])
+    books_list = container.find_all('div', class_='elementList')
+    for book in books_list:
+        left = book.find('div', class_='left')
+        book_img_url = left.find('img')['src']
+        book_url = 'www.goodreads.com/{}'.format(left.find('a', class_='bookTitle')['href'])
+        author_name = left.find('a', class_='authorName').span.text
+        author_url = left.find('a', class_='authorName')['href']
+        grey_text = left.find('span', class_='greyText smallText').text.strip().split('\n')
+        book_avg_rating = locale.atof(grey_text[0].rstrip('—').strip().split()[2])
+        book_ratings_count = locale.atoi(grey_text[1].strip().split()[0])
+        book_published_year = locale.atoi(grey_text[2].strip().split()[1])
+        category_name = s.find('div', class_='breadcrumbs').find_all('a')[-1]['href'].split('/')[-1]
+
+    # add the record to the database
+
+
+def parse_args():
+    """
+    Parse command line parameters and return them as a list.
+    """
+    parser = argparse.ArgumentParser(
+        prog="Books List",
+        description="Scan goodreads to generate a list of books for various genres."
+    )
+
+    parser.add_argument('-l', '--list', action='store_true',
+                        default=False, help='list all genres')
+
+    args = parser.parse_args()
+    return args
+
 
 def main():
-    pass
+    """
+    Entry point of the program.
+    """
+    args = parse_args()
+    print(args)
 
+
+# =================================================================================
+# Trigger main if this is not an import
+# =================================================================================
 if __name__ == '__main__':
     main()
